b5475a7e5d7399b0b8d6d64cf75efc1f518c779b
Susumu OTA <>
2018-12-10 15:55:54 +0900

    added max_episode_steps.

--- gym_modeestimation/__init__.py
+++ gym_modeestimation/__init__.py
@@ -4,35 +4,41 @@ from gym.envs.registration import register
 register(
     id='ModeEstimationEPS00-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 0.0}
 )
 
 register(
     id='ModeEstimationEPS02-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 0.2}
 )
 
 register(
     id='ModeEstimationEPS04-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 0.4}
 )
 
 register(
     id='ModeEstimationEPS06-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 0.6}
 )
 
 register(
     id='ModeEstimationEPS08-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 0.8}
 )
 
 register(
     id='ModeEstimationEPS10-v0',
     entry_point='gym_modeestimation.modeestimation:ModeEstimationEnv',
+    max_episode_steps=30,
     kwargs={'eps': 1.0}
 )
